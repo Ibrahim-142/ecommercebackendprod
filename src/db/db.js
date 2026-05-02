9fff4dd66f341ea588b56e1d799c35acd45e3416
Ibrahim-142 <ibrahimkhurshid65@gmail.com>
2026-05-02 17:42:15 +0500

    Ready for Vercel

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI); 
-    console.log('MongoDB Atlas connected via Mongoose');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 module.exports = connectDB;
