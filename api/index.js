47dfc69edb6578428f3d68d82a80b296d46046c3
Ibrahim-142 <ibrahimkhurshid65@gmail.com>
2026-05-02 17:48:00 +0500

    Fix project structure for Vercel deployment

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const app = require("../src/app");
+const connectDB = require("../src/db/db");
+
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+
+  return app(req, res);
+};
